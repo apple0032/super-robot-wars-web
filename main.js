90f1c16f9de3abbba067b58fefb72fdcbc63ffac
ken ip <kenip0813@gmail.com>
2025-12-03 22:50:02 +0800

    New 3D game init

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1920,      // 設定固定寬度 (包含地圖和選單)
+        height: 1080,      // 設定固定高度
+        resizable: false, // 關鍵：禁止使用者調整視窗大小
+        useContentSize: true, // 確保內容區域大小符合 width/height
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        autoHideMenuBar: true
+    });
+
+    mainWindow.loadFile('srw_3d.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
