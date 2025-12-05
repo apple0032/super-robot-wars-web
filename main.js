15fc53754b5c8331e2f22efcb01e40cb8cbfb0c5
ken ip <kenip0813@gmail.com>
2025-12-06 04:24:08 +0800

    Update build script and logic

--- main.js
+++ main.js
@@ -2,20 +2,6 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path'); // [新增] 引入 path 模組
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 1920,
-        height: 1080,
-        resizable: false,
-        useContentSize: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            webSecurity: false 
-        },
-        autoHideMenuBar: true
-    });
-
-    // --- [關鍵修改] 判斷遊戲模式 ---
     
     // 1. 取得執行檔名稱 (例如 "SRW_Game.exe" 或 "SRW_Admin_Edition.exe")
     const exeName = path.basename(process.execPath).toLowerCase();
@@ -26,13 +12,29 @@ function createWindow() {
     const isNameAdmin = exeName.includes('admin');         // [新增] 檔名是否包含 "admin"
 
     let mode = 'player';
+    let showMenuBar = true;
 
     if (isDev || hasFlag || isNameAdmin) {
         mode = 'admin';
+        showMenuBar = false
     }
 
     console.log(`Executable: ${exeName}`);
     console.log(`Starting game in mode: ${mode}`);
+
+    const mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        resizable: false,
+        useContentSize: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webSecurity: false 
+        },
+        autoHideMenuBar: showMenuBar
+    });
+
 
     // 傳遞參數給 HTML
     mainWindow.loadFile('srw_3d.html', { query: { mode: mode } });
